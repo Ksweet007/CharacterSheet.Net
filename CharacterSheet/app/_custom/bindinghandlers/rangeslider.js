401016b48c53061073be6848f829ab9808f7e3e7
Kyle Sweet <ksweet007@gmail.com>
2017-02-17 11:55:51 -0500

    Cleaned up slider code

--- CharacterSheet/app/_custom/bindinghandlers/rangeslider.js
+++ CharacterSheet/app/_custom/bindinghandlers/rangeslider.js
@@ -5,63 +5,51 @@
     }
 
     _i.ko.bindingHandlers.rangeslider = {
-        init: function(element, valueAccessor) {
+        init: function(element, valueAccessor,allBindings, viewModel, bindingContext) {
             var settings = _i.$.extend({
                 valueInput  : _i.ko.observable(0),
                 type: 'input'
             }, _i.ko.unwrap(valueAccessor()))
+            var sliderElement = _i.$(element)[0];
 
-        if(settings.type === 'input'){
-            //Single range with input
-            var pmdSlider = _i.$('#pmd-slider-value-input')[0];
-            noUiSlider.create(pmdSlider, {
-                start: [ 1 ],
-                connect: 'lower',
-                tooltips: [wNumb({ decimals: 0 }) ],
-                range: {
-                    'min': 1,
-                    'max': 20
-                },
-        		step: 1,
-        		pips: {
-        			mode: 'steps',
-        			density: 1
-        		}
-            });
+            if(settings.type === 'input'){
+                //var pmdSlider = _i.$('#pmd-slider-value-input')[0];
+                noUiSlider.create(sliderElement,
+                    {
+                        start: [ 1 ],
+                        connect: 'lower',
+                        tooltips: [wNumb({ decimals: 0 }) ],
+                        range: {'min': 1, 'max': 20 },
+                        step: 1,
+                        pips: {mode: 'steps', density: 1}
+                    }
+                );
 
-        	pmdSlider.noUiSlider.on('update', function( values, handle ) {
-                // pmdSlider.value  = values[handle];
-                // settings.valueInput = pmdSlider.value;
-                pmdSlider.value  = values[handle];
-                settings.valueInput(pmdSlider.value);
-        	});
-        }
+            	sliderElement.noUiSlider.on('update', function( values, handle ) {
+                    sliderElement.value  = values[handle];
+                    settings.valueInput(sliderElement.value);
+            	});
+            }
 
-        if(settings.type === 'step'){
-            // single range slider with step
-        	var pmdSliderStep = _i.$('#pmd-slider-step')[0];
-        	noUiSlider.create(pmdSliderStep, {
-        		start: [0],
-        		connect: 'lower',
-        		tooltips: [wNumb({ decimals: 0 }) ],
-        		range: {
-        			'min': [0],
-        			'max': [20]
-        		},
-        		step: 1,
-        		pips: {
-        			mode: 'steps',
-        			density: 1
-        		}
-
-        	});
-
-            pmdSliderStep.noUiSlider.on('update', function( values, handle ) {
-                pmdSliderStep.value  = values[handle];
-                settings.valueInput(pmdSliderStep.value);
-        	});
-
-        }
+            // if(settings.type === 'step'){
+            // 	var pmdSliderStep = _i.$('#pmd-slider-step')[0];
+            // 	noUiSlider.create(pmdSliderStep,
+            //         {
+            //             start: [0],
+            //             connect: 'lower',
+            //             tooltips: [wNumb({ decimals: 0 }) ],
+            //             range: {'min': [0], 'max': [20]},
+            //             step: 1,
+            //             pips: {mode: 'steps', density: 1}
+            // 	   }
+            //     );
+            //
+            //     pmdSliderStep.noUiSlider.on('update', function( values, handle ) {
+            //         pmdSliderStep.value  = values[handle];
+            //         settings.valueInput(pmdSliderStep.value);
+            // 	});
+            //
+            // }
 
 
 
