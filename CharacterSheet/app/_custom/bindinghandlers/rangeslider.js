8ae41c3e43d51efb5cdcecc5b3767a3f5048ee13
Kyle Sweet <ksweet007@gmail.com>
2017-02-17 15:40:30 -0500

    Slider no longer has decimal points

--- CharacterSheet/app/_custom/bindinghandlers/rangeslider.js
+++ CharacterSheet/app/_custom/bindinghandlers/rangeslider.js
@@ -20,6 +20,7 @@
                     start: [ 1 ],
                     connect: 'lower',
                     tooltips: [wNumb({ decimals: 0 }) ],
+                    format: wNumb({ decimals: 0}),
                     range: {'min': 1, 'max': 20 },
                     step: 1,
                     pips: {mode: 'steps', density: 1}
@@ -28,15 +29,15 @@
 
             // When the slider value changes, update the input and span
         	sliderElement.noUiSlider.on('update', function( values, handle ) {
-                valueInput.value  = values[handle];
-                settings.valueInput(valueInput.value);
+                var newInput = values[handle];
+                settings.valueInput(newInput);
         	});
 
             // When the input changes, set the slider value
-            valueInput.addEventListener('change', function(){
-                sliderElement.noUiSlider.set([this.value]);
-                settings.valueInput([this.value]);
-            });
+            // valueInput.addEventListener('change', function(){
+            //     sliderElement.noUiSlider.set([this.value]);
+            //     settings.valueInput([this.value]);
+            // });
 
         }//END INIT
     };
