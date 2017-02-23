9daef5bc1c9383499ea9f3d27b3c010ec11a665c
KyleSweetMeM <kyle.sweet@mem.com>
2017-02-23 17:48:42 -0500

    reasons

---
+++ CharacterSheet.Infrastructure/Data/Contexts/EquipmentDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using CharacterSheet.Core.Model;
+
+namespace CharacterSheet.Infrastructure.Data.Contexts
+{
+    public class EquipmentDbContext : DbContext
+    {
+        public EquipmentDbContext()
+            : base("SweetCharacterSheets")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Armor> Armors { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("ksweetadmin");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            EfMapArmor(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static void EfMapArmor(DbModelBuilder modelBuilder)
+        {
+            var armor = modelBuilder.Entity<Armor>();
+            armor.HasRequired(p => p.Proficiency)
+                .WithMany(p => p.Armors);        
+        }
+    }
+}
---
+++ CharacterSheet/app/confirmdelete/confirmdelete.js
@@ -0,0 +1,30 @@
+﻿define(function (require) {
+    var _i = {
+        ko: require('knockout'),
+        dialog: require('plugins/dialog')
+    };
+    var ConfirmDelete = function () { };
+
+    ConfirmDelete.show = function () {
+        return _i.dialog.show(new ConfirmDelete());
+    };
+
+    ConfirmDelete.prototype.dialogButtonClick = function (btnText, accepted) {
+        /// <summary>Called when Ok and Cancel are clicked</summary>
+        var self = this;
+        var closeResult = { btnText: btnText, accepted: accepted, canceled: !accepted };
+
+        _i.dialog.close(self, closeResult);
+
+    };
+    ConfirmDelete.prototype.dialogEscKey = function (eventObj) {
+        var self = this;
+        self.dialogButtonClick('cancel', false, false);
+    };
+    ConfirmDelete.prototype.dialogEnterKey = function (eventObj) {
+        var self = this;
+        self.dialogButtonClick('save', true, true);
+    };
+
+    return ConfirmDelete;
+});
